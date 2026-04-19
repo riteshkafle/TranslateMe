f8d148ea0aaa3c8bfe0175858a389cceefc558d8
Ritesh Kafle <riteshkafle125@gmail.com>
2026-04-19 13:40:42 -0400

    Initial Commit

---
+++ TranslateApp/TranslateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateAppApp.swift
+//  TranslateApp
+//
+//  Created by Ritesh Kafle on 4/19/26.
+//
+
+import SwiftUI
+
+@main
+struct TranslateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
